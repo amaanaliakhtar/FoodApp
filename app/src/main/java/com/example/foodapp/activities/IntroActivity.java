60d8b86219b7ac4e946849e057c67197eedf7f09
Amaan <amaanaliakhtar@gmail.com>
2022-06-18 10:50:56 +0100

    intro activity layout complete

--- app/src/main/java/com/example/foodapp/activities/IntroActivity.java
+++ app/src/main/java/com/example/foodapp/activities/IntroActivity.java
@@ -1,14 +1,16 @@
-package com.example.foodapp;
+package com.example.foodapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.foodapp.R;
+
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
     }
 }
